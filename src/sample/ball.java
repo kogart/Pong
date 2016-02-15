1a41c27d2d8906db537496fd69e7e312207968d9
kogart <kogart@hotmail.com>
2016-02-15 23:18:15 +0100

    fix av kollision och playerscore
    liten fix på kollisionen innan jag lägger till padkolision, lagt till
    getPlayerScore för att hämta player 1 score.

--- src/sample/ball.java
+++ src/sample/ball.java
@@ -5,8 +5,11 @@ import javafx.scene.image.ImageView;
 public class Ball {
 
     Image image = new Image("sample/images/pad.jpg");
+    private int playerScore = 0;
     private double currentX;
     private double currentY;
+    private double startY;
+    private double startX;
     private double ballSpeedY = 5;
     private double ballSpeedX = 5;
     final double BallMinX = 0;
@@ -15,14 +18,16 @@ public class Ball {
     final double BallMaxY = 520;
 
     public Ball(int startX, int startY){
-        this.currentX = startX;
-        this.currentY = startY;
+        this.startX = startX;
+        this.startY = startY;
     }
     public Image getBall(){return this.image;}
 
     public double getCurrentX(){return this.currentX;}
 
     public double getCurrentY(){return this.currentY;}
+
+    public double getPlayerScore(){return this.playerScore;}
 
 
     public void ballMove() {
@@ -33,9 +38,14 @@ public class Ball {
             this.ballSpeedY = -5;
         }
         if (this.currentX == BallMinX) {
+            this.currentY = this.startY;
+            this.currentX = this.startX;
             this.ballSpeedX = 5;
         }
         else if (this.currentX == BallMaxX) {
+            this.playerScore++;
+            this.currentY = this.startY;
+            this.currentX = this.startX;
             this.ballSpeedX = -5;
         }
         this.currentY += this.ballSpeedY;
